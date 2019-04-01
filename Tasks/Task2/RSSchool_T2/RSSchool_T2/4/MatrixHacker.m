727f94bb5301af6b5e8cd949d0767ab612bf9ac8
Anna Kazhuro <anna.kazhuro@icloud.com>
2019-04-01 20:57:16 +0300

    Tiny URL done

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
@@ -1,29 +1,55 @@
 #import "MatrixHacker.h"
 // your code here
+@interface MatrixHacker()<Character>
 
+@property(nonatomic,assign) NSString* nameOfPerson;
+@property(nonatomic,assign) BOOL isHeClone;
+@property(nonatomic, copy) id<Character>(^anotherBlock)(NSString *name);
+
+@end
 
 @implementation MatrixHacker
 // your code here
-- (NSString *)name {
-    return (@"");
-}
-
-- (BOOL)isClone {
-    return YES;
-}
-
 - (void)injectCode:(id<Character> (^)(NSString *name))theBlock {
-    MatrixHacker* a = [MatrixHacker new];
-    NSString* name = [NSString new];
-    
+    _anotherBlock=theBlock;
 }
 
 - (NSArray<id<Character>> *)runCodeWithData:(NSArray<NSString *> *)names {
-    [names retain];
-    __block NSArray* arr = [NSArray new];
+    MatrixHacker* a = [MatrixHacker new];
+    NSArray<id<Character>> *arr = [NSArray new];
     
+    for (NSString* name in names) {
+        id<Character> value = (id)name;
+        a.nameOfPerson = name;
+    
+        if ([[value class] respondsToSelector:@selector(createWithName:isClone:)]) {
+            [arr arrayByAddingObject:[[value class] createWithName:value.name isClone:value.isClone]];
+        }
+    }
     return arr;
 }
-        
+
+
+//+ (instancetype)createWithName:(NSString *)name isClone:(BOOL)clone {
+//    return [(void)(@""), @""];
+//}
+
+- (BOOL)isClone {
+    MatrixHacker* c = [MatrixHacker new];
+    if ([[c nameOfPerson] isEqualToString:@"Neo"]) {
+        return NO;
+    } else {
+        return YES;
+    }
+}
+
+- (NSString *)name {
+    MatrixHacker* b = [MatrixHacker new];
+    if ([[b nameOfPerson] isEqualToString:@"Neo"]) {
+        return @"Neo";
+    } else {
+        return @"AgentSmith";
+    }
+}
 
 @end
--- Tasks/Task2/RSSchool_T2/RSSchool_T2/5/TinyURL.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/5/TinyURL.m
@@ -2,16 +2,29 @@
 
 @implementation TinyURL
 // your code here
-//- (NSURL *)encode:(NSURL *)originalURL{
-//    [originalURL retain];
-//
-//    NSURL* shortened = [NSURL new];
-//
-//    return;
-//}
+
+- (NSURL *)encode:(NSURL *)originalURL {
+    [originalURL retain];
+    NSData *nsdata = [originalURL dataRepresentation];
+    NSString *base64Encoded = [nsdata base64EncodedStringWithOptions:0];
+    NSURL* shortenedURL = [NSURL URLWithString:base64Encoded];
+    
+    [base64Encoded release];
+    
+    return shortenedURL;
+}
 
 
-//- (NSURL *)decode:(NSURL *)shortenedURLv{
-//    
-//}
+- (NSURL *)decode:(NSURL *)shortenedURL {
+    [shortenedURL retain];
+    NSString* stringFromShortenedURL = [shortenedURL absoluteString];
+    NSData *nsdataFromBase64String = [[NSData alloc] initWithBase64EncodedString:stringFromShortenedURL options:0];
+    NSString *base64Decoded = [[NSString alloc] initWithData:nsdataFromBase64String encoding:NSUTF8StringEncoding];
+    NSURL* fullURL = [NSURL URLWithString:base64Decoded];
+    
+    [base64Decoded release];
+    
+    return fullURL;
+}
+
 @end
