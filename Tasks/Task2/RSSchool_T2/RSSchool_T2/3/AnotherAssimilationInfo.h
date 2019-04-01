a8b6ce10b61e3cfb2116ce40d53343d51d469f08
Anna Kazhuro <anna.kazhuro@icloud.com>
2019-04-01 14:04:49 +0300

    DoomsdayMachine done

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/3/AnotherAssimilationInfo.h
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/3/AnotherAssimilationInfo.h
@@ -5,7 +5,6 @@
 
 @property (nonatomic, assign) NSInteger years;
 @property (nonatomic, assign) NSInteger months;
-@property (nonatomic, assign) NSInteger weeks;
 @property (nonatomic, assign) NSInteger days;
 @property (nonatomic, assign) NSInteger hours;
 @property (nonatomic, assign) NSInteger minutes;
