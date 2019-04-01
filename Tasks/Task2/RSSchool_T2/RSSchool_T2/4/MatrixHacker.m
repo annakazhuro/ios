8ea82a0b49e558b49b60e9bf582c911ff2ee4769
Anna Kazhuro <anna.kazhuro@icloud.com>
2019-04-01 21:35:35 +0300

    Some misunderstandings about MatrixHacker task

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
@@ -5,51 +5,50 @@
 @property(nonatomic,assign) NSString* nameOfPerson;
 @property(nonatomic,assign) BOOL isHeClone;
 @property(nonatomic, copy) id<Character>(^anotherBlock)(NSString *name);
+//@property(nonatomic,assign) NSMutableArray* arrayWithInfo;
 
 @end
 
 @implementation MatrixHacker
 // your code here
 - (void)injectCode:(id<Character> (^)(NSString *name))theBlock {
-    _anotherBlock=theBlock;
+    _anotherBlock = theBlock;
 }
 
 - (NSArray<id<Character>> *)runCodeWithData:(NSArray<NSString *> *)names {
-    MatrixHacker* a = [MatrixHacker new];
     NSArray<id<Character>> *arr = [NSArray new];
     
+    self.isHeClone = true;
     for (NSString* name in names) {
         id<Character> value = (id)name;
-        a.nameOfPerson = name;
-    
+        self.nameOfPerson = name;
+        
         if ([[value class] respondsToSelector:@selector(createWithName:isClone:)]) {
+            if ([name isEqualToString:@"Neo"]) {
+                [arr arrayByAddingObject:[[value class] createWithName:@"Neo" isClone: NO]];
+            }
             [arr arrayByAddingObject:[[value class] createWithName:value.name isClone:value.isClone]];
         }
     }
+    
     return arr;
 }
 
 
 //+ (instancetype)createWithName:(NSString *)name isClone:(BOOL)clone {
-//    return [(void)(@""), @""];
+//    MatrixHacker* obj = [MatrixHacker new];
+//    obj.arrayWithInfo = [NSMutableArray arrayWithArray:[obj.arrayWithInfo arrayByAddingObject:name]];
+//
+//
+//    return obj;
 //}
 
 - (BOOL)isClone {
-    MatrixHacker* c = [MatrixHacker new];
-    if ([[c nameOfPerson] isEqualToString:@"Neo"]) {
-        return NO;
-    } else {
-        return YES;
-    }
+    return false;
 }
 
 - (NSString *)name {
-    MatrixHacker* b = [MatrixHacker new];
-    if ([[b nameOfPerson] isEqualToString:@"Neo"]) {
-        return @"Neo";
-    } else {
-        return @"AgentSmith";
-    }
+    return @"Agent Smith";
 }
 
 @end
