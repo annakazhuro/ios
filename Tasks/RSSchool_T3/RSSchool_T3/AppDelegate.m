fe494ff71796d051ffd1ca53819f1409ff52423a
Anna Kazhuro <anna.kazhuro@icloud.com>
2019-04-15 23:37:10 +0300

    1,3 done. 2 not completed

--- Tasks/RSSchool_T3/RSSchool_T3/AppDelegate.m
+++ Tasks/RSSchool_T3/RSSchool_T3/AppDelegate.m
@@ -12,25 +12,9 @@
   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
   [self.window setRootViewController:[DateMachine new]];
   self.window.backgroundColor = [UIColor whiteColor];
-  [self.window makeKeyAndVisible];
-    
-    UIView* view = [[UIView alloc] init];//initWithFrame:[CGRectMake(<#CGFloat x#>, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)]]
-   
-    UIButton *addButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    addButton.frame = CGRectMake(10, 500, 60, 20);
-    [addButton setTitle:@"Add" forState:UIControlStateNormal];
-    
-    
-    
-    UIButton *subButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    subButton.frame = CGRectMake(140, 500, 60, 20);
-    [addButton setTitle:@"Sub" forState:UIControlStateNormal];
-    [view addSubview:addButton];
-    [view addSubview:subButton];
-    
+  [self.window makeKeyAndVisible];    
   return YES;
 }
-
 
 - (void)applicationWillResignActive:(UIApplication *)application {
 }
